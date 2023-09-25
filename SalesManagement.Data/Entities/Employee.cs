51ca9c9d4e87405be33ccd5af325e2d3bfc1d049
sajjad <sajjad.darvish.yektayi@gmail.com>
2023-09-25 13:30:19 +0330

    Add ToolbarBtnClickHndler and add operations btn

--- SalesManagement.Data/Entities/Employee.cs
+++ SalesManagement.Data/Entities/Employee.cs
@@ -1,10 +1,25 @@
-﻿namespace SalesManagement.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesManagement.Data.Entities
 {
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter {0}.")]
+        [MaxLength(100, ErrorMessage = "{0} cannot be greater than {1}!")]
+        [MinLength(2, ErrorMessage = "{0} cannot be less than {1}!")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter {0}.")]
+        [MaxLength(100, ErrorMessage = "{0} cannot be greater than {1}!")]
+        [MinLength(2, ErrorMessage = "{0} cannot be less than {1}!")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter {0}.")]
+        [MaxLength(100, ErrorMessage = "{0} cannot be greater than {1}!")]
+        [MinLength(10, ErrorMessage = "{0} cannot be less than {1}!")]
+        [EmailAddress(ErrorMessage ="Please enter valid {0}")]
         public string Email { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
